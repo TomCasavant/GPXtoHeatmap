57685520a979d23e8540c77dc955cb46d03986e6
Tom Casavant <tfcasavant@gmail.com>
2019-04-17 17:33:40 -0400

    Added config file, and abstracted code into functions

--- heatmap.py
+++ heatmap.py
@@ -1,35 +1,44 @@
 import gpxpy
 import googlemaps
 import os
+from configparser import SafeConfigParser
+
+parser = SafeConfigParser()
+parser.read('config.ini')
+API_KEY = parser.get('GOOGLE', 'API_KEY')
 
 
-API_KEY = 'YOUR_API_KEY'
+"""Loads all gpx files into a list of points"""
+def load_points(folder):
+    coords = []
+    for filename in os.listdir(folder):
+        print (filename)
+        if (filename.endswith(".gpx")):
+            gpx_file = open(f'{folder}/' + filename)
+            gpx = gpxpy.parse(gpx_file)
+            for track in gpx.tracks:
+                for segment in track.segments:
+                    for point in segment.points:
+                        coords.append([float(point.latitude), float(point.longitude)])
+                        print(f'Point at ({point.latitude}, {point.longitude}) -> {point.elevation}')
+
+    return (coords)
 
 
+def get_outline():
+    with open('map-outline.txt', 'r') as file:
+        outline = file.read()
+    return outline
 
-coords = []
-
-for filename in os.listdir('gpx'):
-	print (filename)
-	gpx_file = open('gpx/' + filename)
-	gpx = gpxpy.parse(gpx_file)
-	for track in gpx.tracks:
-	    for segment in track.segments:
-	        for point in segment.points:
-	            coords.append([float(point.latitude), float(point.longitude)])
-	            print(f'Point at ({point.latitude}, {point.longitude}) -> {point.elevation}')
-
-
-with open('map-outline.txt', 'r') as file:
-	content = file.read()
-
-
-def generate_html(points):
-    f = open("map.html", "w")
+def generate_html(points, file_out):
+    f = open(f"{file_out}.html", "w")
+    outline = get_outline()
     google_points = ",\n".join([f"new google.maps.LatLng({point[0]}, {point[1]})" for point in points])
-    newFile = content.replace("LIST_OF_POINTS", google_points).replace("API_KEY", API_KEY)
-    f.write(newFile)
+    updated_content = outline.replace("LIST_OF_POINTS", google_points).replace("API_KEY", API_KEY)
+    f.write(updated_content)
     f.close()
 
 
-generate_html(coords)
+if __name__ == '__main__':
+    points = load_points("gpx")
+    generate_html(points, "map")
