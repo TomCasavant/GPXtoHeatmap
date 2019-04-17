11b57834044b3112280e22d46af1fff7bb77f670
Tom Casavant <tfcasavant@gmail.com>
2019-04-17 17:19:50 -0400

    Removed embedded html to new file

--- heatmap.py
+++ heatmap.py
@@ -1,7 +1,9 @@
 import gpxpy
 import googlemaps
 import os
-API_KEY = 'YOUR_KEY_HERE'
+
+
+API_KEY = 'YOUR_API_KEY'
 
 
 
@@ -17,120 +19,16 @@ for filename in os.listdir('gpx'):
 	            coords.append([float(point.latitude), float(point.longitude)])
 	            print(f'Point at ({point.latitude}, {point.longitude}) -> {point.elevation}')
 
-html = """
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Heatmaps</title>
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-      #floating-panel {
-        position: absolute;
-        top: 10px;
-        left: 25%;
-        z-index: 5;
-        background-color: #fff;
-        padding: 5px;
-        border: 1px solid #999;
-        text-align: center;
-        font-family: 'Roboto','sans-serif';
-        line-height: 30px;
-        padding-left: 10px;
-      }
-      #floating-panel {
-        background-color: #fff;
-        border: 1px solid #999;
-        left: 25%;
-        padding: 5px;
-        position: absolute;
-        top: 10px;
-        z-index: 5;
-      }
-    </style>
-  </head>
 
-  <body>
-    <div id="floating-panel">
-      <button onclick="toggleHeatmap()">Toggle Heatmap</button>
-      <button onclick="changeGradient()">Change gradient</button>
-      <button onclick="changeRadius()">Change radius</button>
-      <button onclick="changeOpacity()">Change opacity</button>
-    </div>
-    <div id="map"></div>
-    <script>
+with open('map-outline.txt', 'r') as file:
+	content = file.read()
 
-      // This example requires the Visualization library. Include the libraries=visualization
-      // parameter when you first load the API. For example:
-      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=visualization">
 
-      var map, heatmap;
-
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
-          center: {lat: 40, lng: -83},
-          mapTypeId: 'roadmap'
-        });
-
-        heatmap = new google.maps.visualization.HeatmapLayer({
-          data: getPoints(),
-          map: map,
-	  maxIntensity: 25,
-	  radius: 5,
-	  opacity:.4
-        });
-      }
-
-      function toggleHeatmap() {
-        heatmap.setMap(heatmap.getMap() ? null : map);
-      }
-     function changeGradient() {
-        var gradient = [
-          'rgba(0, 255, 255, 0)',
-          'rgba(0, 255, 255, 1)',
-          'rgba(0, 191, 255, 1)',
-          'rgba(0, 127, 255, 1)',
-          'rgba(0, 63, 255, 1)',
-          'rgba(0, 0, 255, 1)',
-          'rgba(0, 0, 223, 1)',
-          'rgba(0, 0, 191, 1)',
-          'rgba(0, 0, 159, 1)',
-          'rgba(0, 0, 127, 1)',
-          'rgba(63, 0, 91, 1)',
-          'rgba(127, 0, 63, 1)',
-          'rgba(191, 0, 31, 1)',
-          'rgba(255, 0, 0, 1)'
-        ]
-        heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
-      }
-
-      function changeRadius() {
-        heatmap.set('radius', heatmap.get('radius') ? null : 20);
-      }
-
-      function changeOpacity() {
-        heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
-      }
-      function getPoints() {
-        return [
-"""
 def generate_html(points):
     f = open("map.html", "w")
-    f.write(html)
-    for point in points:
-        f.write(f"new google.maps.LatLng({point[0]}, {point[1]}),\n")
-    f.write('];}</script><script async defer src=\"https://maps.googleapis.com/maps/api/js?key='+ API_KEY + '&libraries=visualization&callback=initMap\"></script></body></html>')
+    google_points = ",\n".join([f"new google.maps.LatLng({point[0]}, {point[1]})" for point in points])
+    newFile = content.replace("LIST_OF_POINTS", google_points).replace("API_KEY", API_KEY)
+    f.write(newFile)
     f.close()
 
 
